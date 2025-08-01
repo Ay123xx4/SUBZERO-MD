9c78fc8492a11a8c60a758e91162d2893982503b
Ay123xx4 <167029419+Ay123xx4@users.noreply.github.com>
2025-08-01 03:32:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;bFFnxSyA#UmpbCvuFWlJkv_cjZ2pSwfrIRHJWJx8Nrilqne-HU3Q", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "263779079858", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
